c833db71996445f1f0ce824149abe68da6736855
DKhorkov <dkhorkov.dev@gmail.com>
2026-04-01 17:22:35 +0300

    Фикс миграции

--- migrations/20260330171139_message_is_read.sql
+++ migrations/20260330171139_message_is_read.sql
@@ -14,5 +14,11 @@ CREATE TABLE IF NOT EXISTS messages_statuses
 CREATE INDEX messages_statuses_message_id_idx ON messages_statuses (message_id);
 CREATE INDEX messages_statuses_user_id_idx ON messages_statuses (user_id);
 
+INSERT INTO messages_statuses (message_id, user_id, is_read)
+SELECT messages.id as message_id, users.id as user_id, true
+FROM messages
+         INNER JOIN chats_members ON messages.chat_id = chats_members.chat_id
+         INNER JOIN users ON chats_members.user_id = users.id;
+
 -- +goose Down
 DROP TABLE IF EXISTS messages_statuses;
