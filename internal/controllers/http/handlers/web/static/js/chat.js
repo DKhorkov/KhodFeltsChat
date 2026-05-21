a6e6a63473ab64544bc73a3fc659b7adc7d1d6ca
Dmitriy Khorkov <dskhorkov@avito.ru>
2026-05-21 11:33:43 +0300

    тест

--- internal/controllers/http/handlers/web/static/js/chat.js
+++ internal/controllers/http/handlers/web/static/js/chat.js
@@ -620,8 +620,8 @@ function setupSwipeToCloseChat() {
                 conversation.style.transition = '';
                 conversation.style.transform = '';
                 unlockPageScroll();
-                // closeChat(); // TODO: диагностика iOS — проверяем влияние перерендера
-                // debouncedLoadChats(); // TODO: диагностика iOS — проверяем влияние перерендера
+                closeChat();
+                // debouncedLoadChats(); // TODO: диагностика iOS
             });
         } else {
             // Возвращаем на место:
